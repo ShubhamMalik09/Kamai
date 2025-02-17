244d1e5c5fdb7bc6a8f305d446710e3a9d7c3526
Shubham Malik <shubhammalik.sm05@gmail.com>
2025-02-17 23:33:46 +0530

    footer updated

--- components/ClientWrapper.js
+++ components/ClientWrapper.js
@@ -13,43 +13,43 @@ export default function ClientWrapper({ children }) {
             {children}
         </main>
         <footer className="mt-14">
-          <div className={`flex flex-col ${darkmode ? "text-black bg-white" : "text-white bg-black"}`}>
-            <div className="grid grid-cols-4 p-5">
-              <img src={darkmode ? "kamai-black.png" : "kamai-white.png"} className="w-48 pt-3 pl-5"/>
+          <div className={`flex flex-col ${darkmode ? "text-black bg-white" : "text-black bg-white"}`}>
+            <div className="grid grid-cols-2 md:grid-cols-4 p-5 gap-4">
+              <img src={darkmode ? "kamai-white.png" : "kamai-black.png"} className="w-48 pt-3 pl-5"/>
               <div className="flex flex-col gap-1">
                 <p className="font-semibold pb-2">Products & pricing</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Pricing</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Atlas</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Billing</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Link</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Payments</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Payment Links</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Invoicing</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Payouts</p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Pricing</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Atlas</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Billing</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Link</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Payments</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Payment Links</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Invoicing</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Payouts</span></p>
               </div>
               <div className="flex flex-col gap-1">
                 <p className="font-semibold pb-2">Solutions</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Startups</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Enterprises</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">SaaS</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Retail</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Platforms</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">E-Commerce</p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Startups</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Enterprises</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">SaaS</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Retail</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Platforms</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">E-Commerce</span></p>
                 <p className="font-semibold mt-3 pb-2">Integrations & custom solutions</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Partner ecosystem</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Professional services</p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Partner ecosystem</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Professional services</span></p>
               </div>
               <div className="flex flex-col gap-1">
                 <p className="font-semibold pb-2">Resources</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Guides</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Blog</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Privacy & terms</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Licences</p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Guides</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Blog</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Privacy & terms</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Licences</span></p>
                 <p className="font-semibold mt-3 pb-2">Company</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Jobs</p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Jobs</span></p>
                 <p className="font-semibold mt-3 pb-1">Support</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Contact sales</p>
-                <p className="opacity-80 hover:cursor-pointer hover:opacity-90">Support centre</p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Contact sales</span></p>
+                <p><span className="opacity-80 hover:cursor-pointer hover:opacity-[93]">Support centre</span></p>
               </div>
             </div>
             <div className="flex w-full items-center justify-center p-3">
