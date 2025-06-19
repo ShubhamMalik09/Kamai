2682fd13805dcc66a0ba3f57d367c07fd760e19a
Shubham Malik <shubhammalik.sm05@gmail.com>
2025-06-19 17:07:52 +0530

    update next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"randomuser.me"
-            },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.teams.microsoft.com https://teams.microsoft.com",
+          },
         ],
-    },
-    async headers() {
-        return [
-            {
-            source: '/(.*)',
-            headers: [
-                {
-                key: 'Content-Security-Policy',
-                value: "frame-ancestors 'self' https://teams.microsoft.com"
-                }
-            ]
-            }
-        ];
-    }
-};
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
